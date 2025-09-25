8e80c300094008373da3549ab7e6bbb2e6c61950
miftahulhudario <119643634+miftahulhudario@users.noreply.github.com>
2025-09-25 09:35:37 +0700

    Update effect.js

--- effect.js
+++ effect.js
@@ -179,6 +179,7 @@ $('document').ready(function(){
 			if(i==50){
 				$("p:nth-child(49)").fadeOut('slow').promise().done(function () {
 					$('.cake').fadeIn('fast');
+					$('.memory-gallery').fadeIn('slow');
 				});
 				
 			}
